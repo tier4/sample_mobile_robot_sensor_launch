e106da46a8600319b06abe1a7d58f5511137bf53
prds1lg <prds1lg@tier4.jp>
2025-02-21 18:53:58 +0900

    fix diagnostics settings and add topic monitor for lidar
    Signed-off-by: prds1lg <prds1lg@tier4.jp>

--- sample_mobile_robot_sensor_launch/launch/topic_state_monitor.launch.py
+++ sample_mobile_robot_sensor_launch/launch/topic_state_monitor.launch.py
@@ -18,6 +18,45 @@ from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
+    # Topic Monitor For LiDAR 
+    topic_state_monitor_lidr_top = ComposableNode(
+        package="topic_state_monitor",
+        plugin="topic_state_monitor::TopicStateMonitorNode",
+        name="topic_state_monitor_lidar_top",
+        parameters=[
+            {
+                "topic": "/sensing/lidar/top/pointcloud_before_sync",
+                "topic_type": "sensor_msgs/msg/PointCloud2",
+                "best_effort": True,
+                "diag_name": "lidar_top_topic_status",
+                "warn_rate": 5.0,
+                "error_rate": 1.0,
+                "timeout": 1.0,
+                "window_size": 10,
+            },
+        ],
+        extra_arguments=[{"use_intra_process_comms": True}],
+    )
+
+    topic_state_monitor_lidr_front = ComposableNode(
+        package="topic_state_monitor",
+        plugin="topic_state_monitor::TopicStateMonitorNode",
+        name="topic_state_monitor_lidar_front",
+        parameters=[
+            {
+                "topic": "/sensing/lidar/front/pointcloud_before_sync",
+                "topic_type": "sensor_msgs/msg/PointCloud2",
+                "best_effort": True,
+                "diag_name": "lidar_front_topic_status",
+                "warn_rate": 5.0,
+                "error_rate": 1.0,
+                "timeout": 1.0,
+                "window_size": 10,
+            },
+        ],
+        extra_arguments=[{"use_intra_process_comms": True}],
+    )
+
     # Topic Monitor For IMU
     topic_state_monitor_imu = ComposableNode(
         package="topic_state_monitor",
@@ -65,6 +104,8 @@ def generate_launch_description():
         package="rclcpp_components",
         executable="component_container",
         composable_node_descriptions=[
+            topic_state_monitor_lidr_top,
+            topic_state_monitor_lidr_front,
             topic_state_monitor_imu,
             topic_state_monitor_vehicle_voice_alert_system,
         ],
