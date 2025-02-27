2e191c73b47de0a5bf7add784edeed62783ac27e
Azumi Suzuki <azumi.suzuki@tier4.jp>
2025-02-27 18:20:10 +0900

    remove unused topic monitor
    Signed-off-by: Azumi Suzuki <azumi.suzuki@tier4.jp>

--- sample_mobile_robot_sensor_launch/launch/topic_state_monitor.launch.py
+++ sample_mobile_robot_sensor_launch/launch/topic_state_monitor.launch.py
@@ -77,26 +77,6 @@ def generate_launch_description():
         extra_arguments=[{"use_intra_process_comms": True}],
     )
 
-    # Topic Monitor For vehicle_voice_alert_system
-    topic_state_monitor_vehicle_voice_alert_system = ComposableNode(
-        package="topic_state_monitor",
-        plugin="topic_state_monitor::TopicStateMonitorNode",
-        name="topic_state_monitor_vehicle_voice_alert_system",
-        parameters=[
-            {
-                "topic": "/vehicle_voice_alert_system/",
-                "topic_type": "sensor_msgs/msg/Imu",
-                "best_effort": True,
-                "diag_name": "vehicle_voice_alert_system_topic_status",
-                "warn_rate": 5.0,
-                "error_rate": 1.0,
-                "timeout": 1.0,
-                "window_size": 10,
-            },
-        ],
-        extra_arguments=[{"use_intra_process_comms": True}],
-    )
-
     # set container to run all required components in the same process
     container = ComposableNodeContainer(
         name="topic_state_monitor_container",
@@ -107,7 +87,6 @@ def generate_launch_description():
             topic_state_monitor_lidar_top,
             topic_state_monitor_lidar_front,
             topic_state_monitor_imu,
-            topic_state_monitor_vehicle_voice_alert_system,
         ],
         output="screen",
     )
